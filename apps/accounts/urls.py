b77e0ba6a10f9337023d9cfc94d8a78d99b6dd39
Khalil1T <tolonbekovkhalil@mail.ru>
2024-02-17 16:31:23 +0600

    Khalil apps.accounts.views urls

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from apps.accounts.views import LoginAPIView
+from apps.accounts.views import LoginAPIView, SellerListAPIView, RegisterView
 
 urlpatterns = [
     path('login/', LoginAPIView.as_view(), name='login'),
+    path('sellers/', SellerListAPIView.as_view(), name='seller-list'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
