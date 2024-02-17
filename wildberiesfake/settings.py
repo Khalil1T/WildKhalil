f8fd5bab839c1ce1a21ea42c9ab8679c35a0c56a
Khalil1T <tolonbekovkhalil@mail.ru>
2024-02-17 22:47:49 +0600

    Исправил key

--- wildberiesfake/settings.py
+++ wildberiesfake/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wnoh&pb44^u@suxw&nmsnyx8$81lx#!ajb@)-&39%!8xl=25fw'
+SECRET_KEY = 'django-insecure-wnoh&pb44^uqqqkhalilqquxw&nmsnyx8$81lx#!ajb@)-&39%!8xl=25fw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
