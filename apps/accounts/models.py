93479aa666cec5741b5a182b1f14563e3b8c9a77
Khalil1T <tolonbekovkhalil@mail.ru>
2024-02-17 15:21:39 +0600

    Khalil serializers

---
+++ apps/accounts/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from .managers import CustomUserManager
+
+
+class MyUser(models.Model):
+    username = None
+    email = models.EmailField('email address', unique=True)
+    password = models.CharField(max_length=255, null=False, blank=False)
+
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    is_Seller = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+class User(MyUser):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    second_name = models.CharField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    adress = models.CharField(max_length=255, null=True, blank=True)
+    card_number = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}, {self.id}'
