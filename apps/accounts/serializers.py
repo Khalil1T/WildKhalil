e01766ebdbb9f5b97cd96031ad5da95fcfb83fa1
Khalil1T <tolonbekovkhalil@mail.ru>
2024-02-17 15:32:20 +0600

    Khalil accounts Serializers part 1

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,4 +1,10 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from apps.accounts.models import User
+
 
 class MyTokenSerializer(TokenObtainPairSerializer):
 
@@ -8,3 +14,50 @@ class MyTokenSerializer(TokenObtainPairSerializer):
         token['email'] = user.email
         token['is_Seller'] = user.is_Seller
         return token
+
+class UserSerializers(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password]
+    )
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True,
+    )
+    name = serializers.CharField(
+        required=True
+    )
+    second_name = serializers.CharField(
+        required=True
+    )
+    addres = serializers.CharField(
+        required=True
+    )
+    phone = serializers.CharField(
+        required=True
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'email',
+            'name',
+            'second_name',
+            'address',
+            'phone',
+            'password',
+            'password2',
+        ]
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {'password': 'Password fields didn`t match'}
+            )
+        return attrs
