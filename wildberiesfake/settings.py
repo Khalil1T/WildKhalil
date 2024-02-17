df464883c98d646f8b217d5723f186056d1c7f48
Your Name <you@example.com>
2024-02-17 16:46:18 +0600

    product

--- wildberiesfake/settings.py
+++ wildberiesfake/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'apps.accounts'
+    'apps.accounts',
+    'apps.product',
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
